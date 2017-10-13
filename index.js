fa25fb24e08511cba311b297e8245dcb2a13abd1
Blake Parkinson <wizardplow@gmail.com>
2017-10-13 16:10:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('LAWkR7hoq05U0zB5WFvaDCldo57oDf3f'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
